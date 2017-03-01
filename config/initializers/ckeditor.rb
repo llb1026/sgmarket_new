8e486a7f88a843159475e92440505c318b862ff0
SGhyun <sghiroo@sogang.ac.kr>
2017-03-01 15:28:38 +0000

    ver0.1

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -9,7 +9,7 @@ Ckeditor.setup do |config|
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
-  config.image_file_types = %w(jpg jpeg png gif tiff)
+  # config.image_file_types = %w(jpg jpeg png gif tiff)
 
   # Allowed flash file types for upload.
   # Set to nil or [] (empty array) for all file types
