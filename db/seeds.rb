77de34db2e71b7c6c1f2f3e71f7697561faa3ac8
SGhyun <sghiroo@sogang.ac.kr>
2017-03-02 01:08:26 +0000

    배포용

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Post.destroy_all
-
-30.times do
-  Post.create!(
-      {
-          user_id: User.all.ids.sample,
-          title: "#{Faker::Internet.domain_word}",
-          category: ['의류', '화장품', '서적', '쿠폰/티켓', '기타'].sample,
-          price: rand(100..10000),
-          contact: "#{Faker::PhoneNumber.cell_phone}",
-          detail: "#{Faker::Hacker.say_something_smart}"
-      }
-  )
-end
+# Post.destroy_all
+#
+# 30.times do
+#   Post.create!(
+#       {
+#           user_id: User.all.ids.sample,
+#           title: "#{Faker::Internet.domain_word}",
+#           category: ['의류', '화장품', '서적', '쿠폰/티켓', '기타'].sample,
+#           price: rand(100..10000),
+#           contact: "#{Faker::PhoneNumber.cell_phone}",
+#           detail: "#{Faker::Hacker.say_something_smart}"
+#       }
+#   )
+# end
